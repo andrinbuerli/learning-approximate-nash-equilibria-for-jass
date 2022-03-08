fix kl

lib/mu_zero/trainer.py
@@ -221,7 +221,7 @@ class MuZeroTrainer:
                 policy_loss += policy_ce
 
                 # ---------------Logging --------------- #
-                policy_target = tf.cast(self.clip_probability_dist(policies_target[:, 0]), tf.float32)
+                policy_target = tf.cast(self.clip_probability_dist(policies_target[:,  i + 1]), tf.float32)
                 policy_kl_divergence_per_sample = tf.reduce_sum(
                     policy_target * tf.math.log(policy_target / self.clip_probability_dist(policy_estimate)), axis=1)
                 policy_kls = policy_kls.write(i+1, tf.reduce_mean(policy_kl_divergence_per_sample, name="kl_mean"))
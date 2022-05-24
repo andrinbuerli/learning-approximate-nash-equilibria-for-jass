dd06d67fffc440b7d0ea15d23c41bef3e8a57048
andrinbuerli <andrin.buerli@hslu.ch>
2022-05-24 09:38:27 +0200

    refactor

--- lib/environment/parallel_jass_environment.py
+++ lib/environment/parallel_jass_environment.py
@@ -228,7 +228,7 @@ class ParallelJassEnvironment:
             network_path,
             check_move_validity=True,
             reanalyse_fraction: float = 0,
-            continuous_games_without_reload: int = 16,
+            continuous_games_without_reload: int = 2,
             reanalyse_data_path="/data"):
         self.continuous_games_without_reload = continuous_games_without_reload
         self.reanalyse_data_path = reanalyse_data_path
